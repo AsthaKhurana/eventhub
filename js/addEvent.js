a2365b6f9206a3ca92df3b0c48364bfec7d48445
AsthaKhurana <astha@wireless-10-104-167-136.umd.edu>
2018-12-07 17:33:05 -0500

    Trying to get the addevent to work

--- js/addEvent.js
+++ js/addEvent.js
@@ -1,15 +1,60 @@
+// var events = {
+//   [
+//     number:1,
+//     name:"OAS Charity Drive",
+//     date:new Date(2018, 6, 12),
+//     location:"SSIS SIS Donation Box 2",
+//     description:"Group: Organization of Arab Students"
+//   ],
+//   [
+//     number:1,
+//     name:"SORC Locker Storage",
+//     date:new Date(2018, 6, 12),
+//     location:"SSIS Terrapod Room AA",
+//     description:"Group: The Stamp: ACTI: Student Org Resource Center"
+//   ]
+// };
 
-Date.setDate() // set date
-//add event form
+//adding new event
+
+var data='';
+var json_str;
+var str='';
+
+var formToJSON = elements => [].reduce.call(elements, (data, element) => {
+
+if (isValidElement(element)) {
+    data[element.name] = element.value;
+  }
+  return data;
+
+}, {});
+
+var isValidElement = element => {
+  return element.name && element.value;
+};
+
+//function for submit button
+function handleFormSubmit(event) {
+  //console.log(" ' " + "hi" + " ' ")
+  event.preventDefault();
+  //data is the json output
+  data = formToJSON(form.elements);
+  console.log(data);
+  console.log(typeof data);
+  //appending it to the html file
+  const dataContainer = document.getElementsByClassName('display')[0];
+  //stringfying the json object
+  json_str = JSON.stringify(data);
+  console.log("json_str = " + json_str);
+
+  var obj = JSON.parse(json_str);
+  document.getElementById("parsed").innerHTML = obj.name + ", " + obj.description+"," +obj.fromTime + "," +obj.toTime;
+  dataContainer.textContent = json_str;
+};
+
+//calling function for submit button
+
 var form = document.getElementById('addNew');
-
-
-//Clear All button
-var clrButton = document.getElementById('clr');
-//clear-all button function
-clrButton.addEventListener(form.reset(), clear);
-
-//event description textarea
-var desc = document.getElementById('desc');
-//
-
+console.log(form);
+form.addEventListener('submit', handleFormSubmit);
