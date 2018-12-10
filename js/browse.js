9f62a6ff54a1ee1dfbbf86c37fc724ff9c8ee0f0
AsthaKhurana <astha@wireless-10-104-167-136.umd.edu>
2018-12-10 15:13:39 -0500

    changes to browse.html

--- js/browse.js
+++ js/browse.js
@@ -1,28 +1,100 @@
 var events = [
 	{
+	eventId: 1,
 	facebook: '',
 	twitter: '',
 	instagram: '',
-	uploadImg: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350',
+	uploadImg: 'https://i0.wp.com/www.methodsunsound.com/wp-content/uploads/2017/02/logan-imax-poster.jpg',
 	name: 'logan Movie',
-	description: 'Some quick example text to build on the card te and the xmas',
+	description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
 	date: '',
 	fromTime: '5:00 pm',
 	toTime: '8:00 pm',
-	place: 'stamp',
+	place: 'hoff theatre',
 	tags: ['movie', 'stamp']
 	},
 	{
+	eventId: 2,
 	facebook: '',
 	twitter: '',
 	instagram: '',
-	uploadImg: 'https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg',
-	name: 'Phul bharey angaarey',
-	description: 'Some quick example text to build on the card title and it seems like trimmng is working',
+	uploadImg: 'https://thenypost.files.wordpress.com/2014/06/shutterstock_145698575.jpg?quality=90&strip=all',
+	name: 'Mountain climbing',
+	description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
 	date: '',
 	fromTime: '5:00 pm',
 	toTime: '8:00 pm',
-	place: 'stamp',
+	place: 'Iron Mountain',
+	tags: ['movie', 'stamp']
+	},
+		{
+	eventId: 3,
+	facebook: '',
+	twitter: '',
+	instagram: '',
+	uploadImg: 'https://media2.fdncms.com/connectsavannah/imager/u/original/8732969/inc2.jpg',
+	name: 'Incredibles 2 Movie',
+	description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+	date: '',
+	fromTime: '5:00 pm',
+	toTime: '8:00 pm',
+	place: 'Hoff Theatre',
+	tags: ['movie', 'stamp']
+	},
+		{
+	eventId: 4,
+	facebook: '',
+	twitter: '',
+	instagram: '',
+	uploadImg: 'https://lovelace-media.imgix.net/getty/159735146.jpg',
+	name: 'Katy Perry Concert',
+	description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+	date: '',
+	fromTime: '5:00 pm',
+	toTime: '8:00 pm',
+	place: 'Mckeldein Mall',
+	tags: ['movie', 'stamp']
+	},
+		{
+	eventId: 5,
+	facebook: '',
+	twitter: '',
+	instagram: '',
+	uploadImg: 'https://cdn-images-1.medium.com/max/665/1*-8YnXeeBod1mOgjbvNo8sw.jpeg',
+	name: 'Therapy with Dogs',
+	description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+	date: '',
+	fromTime: '5:00 pm',
+	toTime: '8:00 pm',
+	place: 'Mckeldein Mall',
+	tags: ['movie', 'stamp']
+	},
+		{
+	eventId: 6,
+	facebook: '',
+	twitter: '',
+	instagram: '',
+	uploadImg: 'https://www.tourismbonaire.com/public_docs/activities/kayaking-lg.jpg',
+	name: 'Kayaking',
+	description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+	date: '',
+	fromTime: '5:00 pm',
+	toTime: '8:00 pm',
+	place: 'SPE Dive School',
+	tags: ['movie', 'stamp']
+	},
+		{
+	eventId: 7,
+	facebook: '',
+	twitter: '',
+	instagram: '',
+	uploadImg: 'https://images.sidearmsports.com/umterps.com/images/2017/9/1/EFGGSOKHMDHVNOL.20170901171458.jpg',
+	name: 'Maryland vs Ohio',
+	description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+	date: '',
+	fromTime: '5:00 pm',
+	toTime: '8:00 pm',
+	place: 'Football field',
 	tags: ['movie', 'stamp']
 	}
 ];
@@ -52,11 +124,118 @@ $( "div .card-container" ).html(function() {
   	var imageBg = 'background: url(' + events[i]["uploadImg"] + ') 20% 1% / cover no-repeat;';
   	var place = capitalizeFirstLetter(events[i]['place']);
 
-  cardGroup += "<article id='card' class='card'><div class='wrapper' style='" + imageBg + "'><div class='date'><div class='black-bg'><span class='week-day'>TUE</span></div><div class='white-bg'><span class='day'>15</span><span class='month'>DEC</span></div></div></div><div class='card-body'><h5 class='card-title'>" + name + "</h5><div class='location'><img class='location-icon' src='assets/img/icons/location-icon.png' alt='Card image cap'><span class='location-placeholder'>" + place + "</span></div><div class='time'><img class='time-icon' src='assets/img/icons/clock-icon.png' alt='Card image cap'><span class='time-placeholder'>" + time + "</span></div><p class='card-text'>" + description + "</p></article>";
+  cardGroup += "<article id='card' data-id='" + events[i]['eventId'] + "' class='card'><div class='wrapper' style='" + imageBg + "'><div class='date'><div class='black-bg'><span class='week-day'>TUE</span></div><div class='white-bg'><span class='day'>15</span><span class='month'>DEC</span></div></div></div><div class='card-body'><h5 class='card-title'>" + name + "</h5><div class='location'><img class='location-icon' src='assets/img/icons/location-icon.png' alt='Card image cap'><span class='location-placeholder'>" + place + "</span></div><div class='time'><img class='time-icon' src='assets/img/icons/clock-icon.png' alt='Card image cap'><span class='time-placeholder'>" + time + "</span></div><p class='card-text'>" + description + "</p></article>";
 
 	}
 
 	return cardGroup;
 });
 
+// cardGroup += "<article id='card' class='card'><div class='wrapper' style='" + imageBg + "'><div class='date'><div class='black-bg'><span class='week-day'>TUE</span></div><div class='white-bg'><span class='day'>15</span><span class='month'>DEC</span></div></div></div><div class='card-body'><h5 class='card-title'>" + name + "</h5><div class='location'><img class='location-icon' src='assets/img/icons/location-icon.png' alt='Card image cap'><span class='location-placeholder'>" + place + "</span></div><div class='time'><img class='time-icon' src='assets/img/icons/clock-icon.png' alt='Card image cap'><span class='time-placeholder'>" + time + "</span></div><p class='card-text'>" + description + "</p></article>";
 
+// Search list below
+
+var eventNames = ["Logan Movie", "Incredibles", "Love lockes"];
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          a.setAttribute("id", "event-suggestions");
+          a.setAttribute("data-id", "event-suggestions");
+          /*make the matching letters bold:*/
+          b.innerHTML = '<img class="event-icon" src="assets/img/icons/event-icon.png" alt="event-icon">';
+          b.innerHTML += "<span id='event-text'> <strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'> </span>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
+
+
+autocomplete(document.getElementById("myInput"), eventNames);
